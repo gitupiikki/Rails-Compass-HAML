67722ece258816848e93da03af69cb7002c83822
Janne Nygård <j.t.nygard@gmail.com>
2011-04-06 19:02:29 +0300

    rails bundled

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3CompassHAML::Application.config.secret_token = 'd8904964c978ede63199f72cd30383d82476e76bc18de821ff24515e6b4329b2dfee84a8011e79fe9c6c1bac630b01eab4b21385a0681bc5ed0f089c14b63ea7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rails3CompassHAML::Application.config.session_store :cookie_store, :key => '_Rails-3-Compass-HAML_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rails3CompassHAML::Application.config.session_store :active_record_store
